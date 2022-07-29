ec15b95e95e475cf443921fe9a135fc4cab59422
Victor Giraldes <victorpgiraldes@gmail.com>
2022-07-29 15:58:58 -0300

    Add codeclimate yml and remove files

--- lib/rubocop/getninjas.rb
+++ lib/rubocop/getninjas.rb
@@ -3,13 +3,7 @@
 require 'pathname'
 require 'yaml'
 
-module RuboCop
-  # RuboCop Getninjas project namespace
+module Rubocop
   module Getninjas
-    PROJECT_ROOT = Pathname.new(__FILE__).parent.parent.parent.expand_path.freeze
-    CONFIG_DEFAULT = PROJECT_ROOT.join('lib', 'rubocop', 'getninjas', 'rubocop.yml').freeze
-    CONFIG = Psych.safe_load(CONFIG_DEFAULT.read).freeze
-
-    private_constant(*constants(false))
   end
 end
